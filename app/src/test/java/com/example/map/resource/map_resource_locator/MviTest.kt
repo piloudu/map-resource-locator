e04609a1b276f95dc03111f1f38c5a07e238a566
Xoan Gonzalez <xoangon@gmail.com>
2022-04-21 13:41:50 +0200

    RED: Test for Logged user intent

--- app/src/test/java/com/example/map/resource/map_resource_locator/MviTest.kt
+++ app/src/test/java/com/example/map/resource/map_resource_locator/MviTest.kt
@@ -1,6 +1,6 @@
 package com.example.map.resource.map_resource_locator
 
-import com.example.map.resource.map_resource_locator.view_model.AppState
+import com.example.map.resource.map_resource_locator.view_model.AppState.*
 import com.example.map.resource.map_resource_locator.view_model.MainActivityUserIntent
 import com.example.map.resource.map_resource_locator.view_model.MainViewModel
 import io.kotest.matchers.shouldBe
@@ -19,11 +19,22 @@ class MviTest {
 
     @DisplayName("after login")
     @Test
-    fun `the inner state is set to MAIN on login`() {
+    fun `the inner state is set to LOADING while downloading the data`() {
         withTestScope {
             mainViewModelTestInstance.sendIntent(MainActivityUserIntent.Login)
             val state = mainViewModelTestInstance.state.value
-            val expectedState = state.copy(innerState = AppState.MAIN)
+            val expectedState = state.copy(innerState = LOADING)
+            state shouldBe expectedState
+        }
+    }
+
+    @DisplayName("on login completed")
+    @Test
+    fun `the inner state is set to MAIN after downloading the data`() {
+        withTestScope {
+            mainViewModelTestInstance.sendIntent(MainActivityUserIntent.Logged)
+            val state = mainViewModelTestInstance.state.value
+            val expectedState = state.copy(innerState = MAIN)
             state shouldBe expectedState
         }
     }
